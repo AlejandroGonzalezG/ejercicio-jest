18a53361cdd467fd3ebe46b1230fe622843794c6
AlejandroGonzalezG <ale.gonzalez.gut@gmail.com>
2022-07-02 22:42:57 +0000

    prueba jest

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ let oneEuroIs = {
     "JPY": 127.9, // japan yen
     "USD": 1.2, // us dollar
     "GBP": 0.8, // british pound
-}
+};
 
 const fromDollarToYen  = function(valueInDollar){
     // convertimos el valor a yenes
-    let valueInYen = fromEuroToDollar(valueInDollar) * 127.9;
+    let valueInEuro = (valueInDollar / oneEuroIs.USD);
+    let valueInYen = valueInEuro * oneEuroIs.JPY;
     // retornamos el valor
     return valueInYen;
 };
 
 const fromEuroToDollar = function(valueInEuro){
     // convertimos el valor a dolares
-    let valueInDollar = valueInEuro * 1.2;
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
     // retornamos el valor
     return valueInDollar;
 };
 
 const fromYenToPound = function(valueInYen){
     // convertimos el valor a pounds
-    let valueInPound = valueInYen * 1.2;
+    let valueInEuro = (valueInYen / oneEuroIs.JPY);
+    let valueInPound = (valueInEuro * oneEuroIs.GBP);
     // retornamos el valor
     return valueInPound;
 };
+
 
 module.exports ={ fromEuroToDollar,fromDollarToYen,fromYenToPound };
--- test.js
+++ test.js
@@ -7,3 +7,19 @@ test("One euro should be 1.206 dollars", function(){
     // hago mi comparacion (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
 })
+
+test("One dollar should be 106.5833 yenes", function(){
+    // importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromDollarToYen(100)).toBe(10658.333333333336); //1 dolar son 106.5833 yenes, entonces 100 dolares deberian ser = ((100 / 1.2) * 127.9)
+})
+
+test("One yen should be 0.006254 pounds", function(){
+    // importo la funcion desde app.js
+    const { fromYenToPound } = require('./app.js')
+    
+    // hago mi comparacion (la prueba)
+    expect(fromYenToPound(100)).toBe(0.6254886630179828); //1 yen son 0.006254 pounds, entonces 100 yenes deberian ser = ((100 / 129.7) *0.8)
+})
